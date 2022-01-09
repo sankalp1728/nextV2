e87e24d6fad24bdd5358509200c75e7f9b5ad438
Sankalp Gupta <sankalp1728@gmail.com>
2022-01-09 18:13:54 +0530

    added isSuperAdmin auth

--- models/companyDetails.js
+++ models/companyDetails.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose")
 const companySchema = new mongoose.Schema({
     name : {
         type : String,
-        required : true
+        required : true,
+        unique : true
     },
 
     locations : [{
